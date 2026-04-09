65f96d72bccced8b3a46ab3a07f03d3717f09ff9
Paul Mars <paul.mars@canonical.com>
2026-04-09 09:47:57 +0200

    feat: collect attributes in DB

--- log2db.py
+++ log2db.py
@@ -2,13 +2,16 @@ import re
 import sqlite3
 from datetime import datetime
 
-relation_re = re.compile(
-    r"opid:\s*(\d+)\s+dpid:\s*(\d+)\s+id:\s*(\d+)\s+id:\s*(\d+)"
+relation_re = re.compile(r"opid:\s*(\d+)\s+dpid:\s*(\d+)\s+id:\s*(\d+)\s+id:\s*(\d+)")
+
+attribute_re = re.compile(
+    r"(\d{4}-\d{2}-\d{2}T[0-9:.]+)\s+rpc-attr\s+pid:\s*(\d+)\s+sm_id:\s*(\d+)\s+([A-Za-z0-9_-]+)\s+key:\s*([A-Za-z0-9_-]+)\s+value:\s*(\d+)"
 )
 
 event_re = re.compile(
     r"(\d{4}-\d{2}-\d{2}T[0-9:.]+)\s+\w+\s+pid:\s*(\d+)\s+sm_id:\s*(\d+)\s+([A-Za-z0-9_-]+)"
 )
+
 
 def parse_log(logfile, dbfile="logs.db"):
     # Setup SQLite
@@ -32,6 +35,16 @@ def parse_log(logfile, dbfile="logs.db"):
             event TEXT
         )
     """)
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS attributes (
+            time TEXT,
+            pid INTEGER,
+            id INTEGER,
+            event TEXT,
+            key TEXT,
+            value TEXT
+        )
+    """)
 
     with open(logfile, "r") as f:
         for line in f:
@@ -41,6 +54,14 @@ def parse_log(logfile, dbfile="logs.db"):
                     cur.execute(
                         "INSERT INTO relations (opid, dpid, o_id, d_id) VALUES (?, ?, ?, ?)",
                         tuple(map(int, m.groups())),
+                    )
+            elif "rpc-attr" in line:
+                m = attribute_re.search(line)
+                if m:
+                    t, pid, sm_id, event, key, value = m.groups()
+                    cur.execute(
+                        "INSERT INTO attributes (time, pid, id, event, key, value) VALUES (?, ?, ?, ?, ?, ?)",
+                        (t, int(pid), int(sm_id), event, key, value),
                     )
             else:
                 m = event_re.search(line)
